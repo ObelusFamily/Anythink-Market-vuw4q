c1cf71df84cf258df03921f681aeaeb61130cf2d
teguh <danielsxiong@gmail.com>
2022-08-12 22:39:25 +0700

    Fix broken image on item preview and item list

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -1,8 +1,9 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import agent from "../agent";
-import { connect } from "react-redux";
-import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import agent from '../agent';
+import { connect } from 'react-redux';
+import { ITEM_FAVORITED, ITEM_UNFAVORITED } from '../constants/actionTypes';
+import placeholder from '../imgs/placeholder.png';
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
@@ -32,13 +33,13 @@ const ItemPreview = (props) => {
   return (
     <div
       className="card bg-dark border-light p-3"
-      style={{ borderRadius: "20px" }}
+      style={{ borderRadius: '20px' }}
     >
       <img
         alt="item"
-        src={item.image}
+        src={item.image !== '' ? item.image : placeholder}
         className="card-img-top item-img"
-        style={{ borderRadius: "20px" }}
+        style={{ borderRadius: '20px' }}
       />
       <div className="card-body">
         <Link to={`/item/${item.slug}`} className="text-white">
